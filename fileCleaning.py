01e7bfe4f932db6d22d3e73ced6d3f38eb50c402
r s <rsreenivasan5@gmail.com>
2022-07-13 21:51:55 -0400

    added files

---
+++ fileCleaning.py
@@ -0,0 +1,39 @@
+import pandas as pd
+from featureExtraction import featureExtraction
+
+data0 = pd.read_csv("Phishing_Mitre_Dataset_Summer_of_AI.csv", encoding="latin-1")
+data0.head()
+features = []
+label = 0
+
+for i in range(0, 1000):
+    url = data0["URL"][i]
+    modified = url[:-3]
+    print(modified)
+    features.append(featureExtraction(modified, label))
+
+feature_names = [
+    "Domain",
+    "Have_IP",
+    "Have_At",
+    "URL_Length",
+    "URL_Depth",
+    "Redirection",
+    "https_Domain",
+    "TinyURL",
+    "Prefix/Suffix",
+    "DNS_Record",
+    "Web_Traffic",
+    "Domain_Age",
+    "Domain_End",
+    "iFrame",
+    "Mouse_Over",
+    "Right_Click",
+    "Web_Forwards",
+    "Label",
+]
+
+featList = pd.DataFrame(features, columns=feature_names)
+featList.head()
+featList.to_csv("res.csv", index=False)
+print(featList)
---
+++ models.py
@@ -0,0 +1,57 @@
+# importing basic packages
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+data0 = pd.read_csv("res.csv")
+data0.head()
+
+data0.describe()
+# Dropping the Domain column
+data = data0.drop(["Domain"], axis=1).copy()
+data = data.sample(frac=1).reset_index(drop=True)
+data.head()
+y = data["Label"]
+X = data.drop("Label", axis=1)
+X.shape, y.shape
+# Splitting the dataset into train and test sets: 80-20 split
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=12
+)
+X_train.shape, X_test.shape
+
+from sklearn.metrics import accuracy_score
+
+# Creating holders to store the model performance results
+ML_Model = []
+acc_train = []
+acc_test = []
+
+# function to call for storing the results
+def storeResults(model, a, b):
+    ML_Model.append(model)
+    acc_train.append(round(a, 3))
+    acc_test.append(round(b, 3))
+
+    # Decision Tree model
+
+
+from sklearn.tree import DecisionTreeClassifier
+
+# instantiate the model
+tree = DecisionTreeClassifier(max_depth=5)
+# fit the model
+tree.fit(X_train, y_train)
+# predicting the target value from the model for the samples
+y_test_tree = tree.predict(X_test)
+y_train_tree = tree.predict(X_train)
+
+# computing the accuracy of the model performance
+acc_train_tree = accuracy_score(y_train, y_train_tree)
+acc_test_tree = accuracy_score(y_test, y_test_tree)
+
+print("Decision Tree: Accuracy on training Data: {:.3f}".format(acc_train_tree))
+print("Decision Tree: Accuracy on test Data: {:.3f}".format(acc_test_tree))
